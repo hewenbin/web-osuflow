e3445c8d122022b5486632fd1b15c56b7f3cb4c5
Wenbin He <hewenbin.495@gmail.com>
2013-09-22 15:05:36 -0400

    Create the directory structure

---
+++ public/scripts/wof-gui.js
@@ -0,0 +1,9 @@
+// web osuflow gui initialization
+function init_gui() {
+	// init tool tips
+	$("#tb-os").tooltip();
+	$("#tb-as").tooltip();
+	$("#tb-lic").tooltip();
+	$("#tb-al").tooltip();
+	$("#tb-set").tooltip();
+}
---
+++ public/scripts/wof-main.js
@@ -0,0 +1,9 @@
+// web osuflow initialization
+function init() {
+	init_gui();
+}
+
+// entry of web osuflow
+$(document).ready(function () {
+	init();
+});
