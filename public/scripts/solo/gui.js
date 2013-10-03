2552d8677585951add4b1b6badaf64905ad5b5d8
Wenbin He <hewenbin.495@gmail.com>
2013-10-02 20:06:09 -0400

    Build settings tools

--- public/scripts/solo/gui.js
+++ public/scripts/solo/gui.js
@@ -16,6 +16,7 @@ function init_gui() {
 
 			$("#tool-os").show();
 			$("#tool-as").hide();
+			$("#tool-set").hide();
 
 			webState = 0;
 			init_os();
@@ -26,6 +27,7 @@ function init_gui() {
 		if (webState !== 1) {
 			$("#tool-os").hide();
 			$("#tool-as").show();
+			$("#tool-set").hide();
 
 			webState = 1;
 			init_as();
@@ -36,14 +38,35 @@ function init_gui() {
 		}
 	});
 
+	$("#tb-set").on('click', function () {
+		if (webState !== 4) {
+			if (webState === 1) {
+				trans_control.detach(field.cubeSB);
+				scene.remove(trans_control.gizmo);
+			}
+
+			$("#tool-os").hide();
+			$("#tool-as").hide();
+			$("#tool-set").show();
+
+			// clear remaining data
+			field.clearVecs();
+			field.clearSeedBoundary();
+
+			webState = 4;
+		}
+	});
+
 	// init one seed tools
 	$("#os-generate").on('click', function () {
 		$(this).button("loading");
 		$("#as-generate").button("loading");
 		socket.emit("genStreamLines", {
 			seeds : field.seeds,
-			direction : 2,
-			maxpoints : 500
+			direction : $("#set-td label.active input").val(),
+			maxpoints : $("#set-number").data('slider').getValue(),
+			stepsize : [$("#set-min").data('slider').getValue(),
+				$("#set-max").data('slider').getValue()]
 		});
 	});
 
@@ -58,13 +81,29 @@ function init_gui() {
 		field.genInCube($("#as-number").data('slider').getValue());
 		socket.emit("genStreamLines", {
 			seeds : field.seeds,
-			direction : 2,
-			maxpoints : 500
+			direction : $("#set-td label.active input").val(),
+			maxpoints : $("#set-number").data('slider').getValue(),
+			stepsize : [$("#set-min").data('slider').getValue(),
+				$("#set-max").data('slider').getValue()]
 		});
+	});
+
+	// init settings tools
+	$("#set-number").slider({min : 10, max : 1000, value : 500}).on("slide", function (event) {
+		$("#set-number-info").html("max points number = " + event.value);
+	});
+
+	$("#set-min").slider({min : 0.2, max : 2, value : 1}).on("slide", function (event) {
+		$("#set-min-info").html("min = " + event.value.toFixed(1) + " ");
+	});
+
+	$("#set-max").slider({min : 2, max : 5, value : 4}).on("slide", function (event) {
+		$("#set-max-info").html("max = " + event.value.toFixed(1));
 	});
 
 	// arrange all tools
 	$("#tool-as").hide();
+	$("#tool-set").hide();
 
 	// init top toggles
 	$("#cm-basic").on('click', function () {
